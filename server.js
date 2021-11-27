11234d08dc8d843fbe237fb3b6afa2c56a512c07
Abdulrahman Salau <bob.salau@gmail.com>
2021-11-27 16:49:53 +0100

    freecode camp solution

---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:timeValue?", async function (req, res) {
+   let {timeValue} = req.params;
+
+   if(!timeValue){
+      timeValue = Date.now()
+   }
+   let isnum = /^\d+$/.test(timeValue);
+   
+   if(isnum){
+    timeValue = parseInt(timeValue)
+   }
+
+   const parsedDate = new Date(timeValue);
+
+   if(parsedDate == "Invalid Date"){
+     return res.json({error: 'Invalid Date'})
+   }
+
+   const unix = parsedDate.valueOf();
+   const utc  = parsedDate.toUTCString();
+
+   return res.json({
+    unix,
+    utc
+   })
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
